5ad6c6a0986da897e64219687c5522a8f055df7d
vexades <ionutnato@gmail.com>
2020-09-20 09:20:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/databinding/User.java
@@ -0,0 +1,19 @@
+package com.example.databinding;
+
+public class User {
+    public String firstname;
+    public String lastname;
+
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+}
